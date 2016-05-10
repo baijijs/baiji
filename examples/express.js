345fe2a63068c71959af2750cecea0324760adb5
lyfeyaj <lyfeyaj@gmail.com>
2016-05-10 11:23:11 +0800

    fix express adapter bugs and enhance compose with error handler functionality

--- examples/express.js
+++ examples/express.js
@@ -1,18 +1,33 @@
 'use strict';
 
 const baiji = require('../');
+const debug = require('debug')('baiji:examples:express');
 
 let app = baiji('myApp');
 
 let ArticlesCtrl = baiji('articles');
 
 ArticlesCtrl.before('index', function(ctx, next) {
-  console.log('before index executed.');
+  debug('before index executed.');
+  setTimeout(function() {
+    next();
+  }, 2000);
+});
+
+ArticlesCtrl.before('*', function(ctx, next) {
+  debug('before * executed.');
   next();
 });
 
 ArticlesCtrl.after('index', function(ctx, next) {
-  console.log('after index executed.');
+  debug('after index executed.');
+  next();
+});
+
+ArticlesCtrl.afterError('*', function(ctx, next) {
+  debug('afterError * executed.');
+  debug(ctx.error);
+  ctx.done(ctx.error);
   next();
 });
 
@@ -22,12 +37,18 @@ ArticlesCtrl.define('index', {
   ],
   http: { verb: 'get', path: '/' }
 }, function(ctx, next) {
+  debug('method executed', ctx._method.fullName());
   ctx.done(ctx.args);
+  // throw new Error('throw error');
+  // return Promise.reject(new Error('promise error'));
+  next();
+});
+
+app.before('*', function(ctx, next) {
+  debug('before all executed.');
   next();
 });
 
 app.use(ArticlesCtrl, { mountpath: '/articles' });
-
-console.log(app.methods[0].fullPath());
 
 app.listen(3005);
