8f14fb6e45cffe88cc6e2c51ce1f56cb9a0c4108
lyfeyaj <lyfeyaj@gmail.com>
2016-05-12 17:06:14 +0800

    fix several bugs

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Application = require('./lib/Application');
 
 module.exports = createApplication;
 
-function createApplication(name) {
-  return new Application(name);
+function createApplication(name, settings) {
+  return new Application(name, settings);
 }
 
 createApplication.Application = Application;
