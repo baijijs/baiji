bdac4b1c2e309bc43d8a7369b8162c323d6b011f
lyfeyaj <lyfeyaj@gmail.com>
2017-02-11 19:40:39 +0800

    fix Controller bug and add more Controller related tests

--- test/Controller.test.js
+++ test/Controller.test.js
@@ -102,4 +102,239 @@ describe('class Controller', function() {
       expect(ctrl.getMountPath()).to.eq('/test');
     });
   });
+
+  describe('configure(nameOrConfigs, methodConfig)', function() {
+    it('should be able to config via object', function() {
+      ctrl.configure({ abc: 1 });
+      expect(ctrl.__configs).to.have.property('abc', 1);
+    });
+
+    it('should be able to config via name and object', function() {
+      ctrl.configure('name', { abc: 1 });
+      expect(ctrl.__configs).to.have.property('name').deep.eq({ abc: 1 });
+    });
+  });
+
+  // Hooks tests
+  [
+    'before',
+    'after',
+    'afterError'
+  ].forEach(function(hookName) {
+    describe(`${hookName}Action(nameOrFn, options)`, function() {
+      it('should raise an error if there is no action method can be found', function() {
+        expect(function() {
+          ctrl[`${hookName}Action`]('non_existed_method');
+        }).to.throw(Error).to.have.property('message', 'No method named \'non_existed_method\' defined');
+      });
+
+      it(`should set ${hookName} actions`, function() {
+        ctrl.filterUser = function() {};
+        ctrl[`${hookName}Action`]('filterUser');
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: [],
+            only: ['*']
+          }
+        });
+      });
+
+      it('should add proper filters by `except` option', function() {
+        ctrl.filterUser = function() {};
+        ctrl[`${hookName}Action`]('filterUser', { except: 'index' });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: ['index'],
+            only: ['*']
+          }
+        });
+
+        ctrl.__hooksConfig = {};
+
+        ctrl[`${hookName}Action`]('filterUser', { except: '*' });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: ['*'],
+            only: []
+          }
+        });
+
+        ctrl.__hooksConfig = {};
+
+        ctrl[`${hookName}Action`]('filterUser', { except: ['index', 'show'] });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: ['index', 'show'],
+            only: ['*']
+          }
+        });
+      });
+
+      it('should add proper filters by `only` option', function() {
+        ctrl.filterUser = function() {};
+        ctrl[`${hookName}Action`]('filterUser', { only: 'index' });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: [],
+            only: ['index']
+          }
+        });
+
+        ctrl.__hooksConfig = {};
+
+        ctrl[`${hookName}Action`]('filterUser', { only: '*' });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: [],
+            only: ['*']
+          }
+        });
+
+        ctrl.__hooksConfig = {};
+
+        ctrl[`${hookName}Action`]('filterUser', { only: ['index', 'show'] });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser`).deep.eq({
+          fn: ctrl.filterUser,
+          options: {
+            except: [],
+            only: ['index', 'show']
+          }
+        });
+      });
+    });
+
+  });
+
+  // Test skip hook methods
+  [
+    'before',
+    'after',
+    'afterError'
+  ].forEach(function(hookName) {
+    let HookName = hookName.replace(/^[a-z]/, function(r) { return r.toUpperCase(); });
+
+    describe(`skip${HookName}Action(nameOrFn, options)`, function() {
+      beforeEach(function() {
+        ctrl.filterUser = function() {};
+        ctrl[`${hookName}Action`]('filterUser');
+      });
+
+      it('should raise an error if there is no action method can be found', function() {
+        expect(function() {
+          ctrl[`skip${HookName}Action`]('non_existed_method');
+        }).to.throw(Error).to.have.property('message', 'No method named \'non_existed_method\' defined');
+      });
+
+      it(`should skip ${hookName} actions`, function() {
+        ctrl[`skip${HookName}Action`]('filterUser');
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          except: ['*'],
+          only: []
+        });
+      });
+
+      it('should skip filters by `except` option', function() {
+        ctrl[`skip${HookName}Action`]('filterUser', { except: 'index' });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['index'],
+          except: []
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser');
+        ctrl[`skip${HookName}Action`]('filterUser', { except: '*' });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['*'],
+          except: []
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser');
+        ctrl[`skip${HookName}Action`]('filterUser', { except: ['index', 'show'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['index', 'show'],
+          except: []
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser', { only: 'index' });
+        ctrl[`skip${HookName}Action`]('filterUser', { except: ['show'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['show'],
+          except: []
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser', { only: 'index' });
+        ctrl[`skip${HookName}Action`]('filterUser', { except: ['index'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['index'],
+          except: []
+        });
+      });
+
+      it('should skip filters by `only` option', function() {
+        ctrl.filterUser = function() {};
+        ctrl[`skip${HookName}Action`]('filterUser', { only: 'index' });
+
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['*'],
+          except: ['index']
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser');
+        ctrl[`skip${HookName}Action`]('filterUser', { only: '*' });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: [],
+          except: ['*']
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser');
+        ctrl[`skip${HookName}Action`]('filterUser', { only: ['index', 'show'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['*'],
+          except: ['index', 'show']
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser', { only: 'index' });
+        ctrl[`skip${HookName}Action`]('filterUser', { only: ['show'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: ['index'],
+          except: ['show']
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser', { only: 'index' });
+        ctrl[`skip${HookName}Action`]('filterUser', { only: ['index'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: [],
+          except: ['index']
+        });
+
+        ctrl.__hooksConfig = {};
+        ctrl[`${hookName}Action`]('filterUser', { only: 'index', except: 'show' });
+        ctrl[`skip${HookName}Action`]('filterUser', { only: ['index'] });
+        expect(ctrl.__hooksConfig).to.have.deep.property(`${hookName}.filterUser.options`).deep.eq({
+          only: [],
+          except: ['show', 'index']
+        });
+      });
+    });
+  });
 });
